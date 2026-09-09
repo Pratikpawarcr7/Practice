3eafb29dc62ad14e11ea88faf15d6b13afbdd7dc
Pratikpawarcr7 <pawarpratikcr7@gmail.com>
2026-09-09 11:53:17 -0400

    Add files via upload

---
+++ Array1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+
+    float Mark1 = 78.98;
+    float Marks2 = 90.99;
+    float Marks3 = 67.89;
+    float Marks4 = 56.90;
+    float Marks5 = 87.56;
+
+    float Marks[5] = {78.98, 90.99, 67.89, 56.90, 87.56};
+
+    printf("%f", Marks);
+
+    return 0;
+}
